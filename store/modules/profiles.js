959a34951e92c518720d20a2f9ee3f8b30aee619
Pierre_T <pierre.tsiakkaros@gmail.com>
2018-12-23 19:19:14 +0100

    #4 :bug: :recycle: fix and clean

--- store/modules/profiles.js
+++ store/modules/profiles.js
@@ -76,35 +76,15 @@ export default {
 
     async updateUserTags({ rootGetters }, tags) {
       const { newTags, existingTags } = tags
-      console.log("​updateUserTags -> existingTags", existingTags)
-      console.log("​updateUserTags -> newTags", newTags)
-      const savedTags = []
+      const savedTags = [];
       for (let tag of newTags) {
-        const existingTagsRef = await fireDb.collection('tags').get()
-        console.log("​updateUserTags -> existingTagsRef", existingTagsRef)
-        for (let doc of existingTagsRef.docs) {
-          const existingTag = doc.data()
-          if (existingTag.label === tag.label) {
-            console.log(`${tag.label} already exists dude!`)
-            savedTags.push(existingTag)
-            console.log('savedTags', savedTags)
-          } else {
-            const newTagRef = await fireDb.collection('tags').doc()
-            savedTags.push({ ...tag, tagId: newTagRef.id })
-            newTagRef.set({ ...tag, tagId: newTagRef.id })
-
-          }
-        }
+        const tagRef = await fireDb.collection('tags').doc()
+        savedTags.push({ ...tag, tagId: tagRef.id })
+        tagRef.set({ ...tag, tagId: tagRef.id })
       }
-
       const allTags = existingTags.concat(savedTags)
-      console.log("​updateUserTags -> allTags", allTags)
       const userId = rootGetters.user.userId
-
       await fireDb.collection('profiles').doc(userId).update({ tags: allTags })
-
     }
-
   }
-
 };
